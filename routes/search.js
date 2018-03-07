aa23edcfd5843b1052314f64664682ae298ddd66
Joe Bockskopf <jbockskopf@g.harvard.edu>
2018-03-06 22:51:23 -0500

    Removing PubMed API key

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 var eUtilsBaseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
-var apiKey = '0cf11408a60d684ff3119267134e6e66f808';
+var apiKey = '';
+var apiKeyParam = '';
+if (apiKey !== '') {
+    apiKeyParam = `&api_key=${apiKey}`
+}
+
 router.get('/', (req, response) => {
 
     // Search Term from the form
@@ -19,7 +24,7 @@ router.get('/', (req, response) => {
         response.redirect('list');
     } else {
         // E-Search request
-        var searchUrl = `${eUtilsBaseUrl}esearch.fcgi?db=PMC&term=${searchTerm}&retmode=json&usehistory=y&api_key=${apiKey}`;
+        var searchUrl = `${eUtilsBaseUrl}esearch.fcgi?db=PMC&term=${searchTerm}&retmode=json&usehistory=y${apiKeyParam}`;
         httpRequest(searchUrl, {json: true}, (err, res, body) => {
                 if (err) { return console.log(err);}
 
@@ -30,7 +35,7 @@ router.get('/', (req, response) => {
                 // Build the e-summary url using the webenv and querykey.
                 var querykey = body.esearchresult.querykey;
                 var webenv = body.esearchresult.webenv;
-                var summaryUrl = `${eUtilsBaseUrl}esummary.fcgi?db=PMC&query_key=${querykey}&WebEnv=${webenv}&retmode=json&retmax=20&api_key=${apiKey}`;
+                var summaryUrl = `${eUtilsBaseUrl}esummary.fcgi?db=PMC&query_key=${querykey}&WebEnv=${webenv}&retmode=json&retmax=20${apiKeyParam}`;
 
                 // E-Summary request
                 httpRequest(summaryUrl, {json: true}, (err, res, body) => {
