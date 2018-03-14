68f1ac31d2e81c98daa3dd6ea770d1735e0ac8b0
Ian Smith <ismith@imprivata.com>
2018-03-14 01:59:05 -0400

    create dockerized postgres database

---
+++ docker/postgres/pubminer-schema.sql
@@ -0,0 +1,12 @@
+-- Initializes the pubminer schema. This is a stub schema and should be replaced with the
+-- schema definition
+
+CREATE TABLE demographic_index(
+    pmc_id character varying(30),
+    participants integer NULL,
+    median_age smallint  NULL
+);
+
+
+
+
