4b4af80afebb84f758e789a80f6f5cebd08279aa
Joe Bockskopf <jbockskopf@g.harvard.edu>
2018-04-15 00:57:27 -0400

    Update items displayed metric.  Hide more button after last item.

--- routes/results.js
+++ routes/results.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 var pubMedQuery = require('../search');
 
-router.get('/', (request, response) => {
+const rowRequestSize = 20;
 
-    const initialRowRequestSize = 20;
+router.get('/', (request, response) => {
 
     // search PubMed for results matching the search terms
     pubMedQuery.search(request.query.searchTerm, (queryResult) => {
@@ -15,19 +15,11 @@ router.get('/', (request, response) => {
                                         "qryResult": queryResult});
         } else {
             // Get summaries for the first set of result rows
-            pubMedQuery.getSummaries(queryResult.webenv, queryResult.querykey, 0, initialRowRequestSize, (results) => {
+            pubMedQuery.getSummaries(queryResult.webenv, queryResult.querykey, 0, rowRequestSize, (results) => {
 
-                var nextRow = initialRowRequestSize + 1;
-                var rowCount = initialRowRequestSize;
-                if (queryResult.itemsFound < initialRowRequestSize) {
-                    nextRow = 0;
-                    rowCount = 0;
-                }
                 // Render the 'results' view using query results
                 response.render('results', {"results": results,
-                                            "qryResult": queryResult,
-                                            "nextRow": nextRow,
-                                            "rowCount": rowCount});
+                                            "qryResult": queryResult});
             });
         }
     });
@@ -36,7 +28,7 @@ router.get('/', (request, response) => {
 router.get('/:webenv/:querykey', function(req, res, next) {
 
     // Get summaries for result rows
-    pubMedQuery.getSummaries(req.params.webenv, req.params.querykey, req.query.start, req.query.count, (results) => {
+    pubMedQuery.getSummaries(req.params.webenv, req.params.querykey, req.query.start, rowRequestSize, (results) => {
 
         // Render the 'list' view using query results, if any
         res.render('resultRows', { "results": results });
