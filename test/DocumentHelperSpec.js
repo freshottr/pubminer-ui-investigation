c36acf9380081ed0b61277ce444b6d21082da95e
Ian Smith <i.m.k.smith@gmail.com>
2018-04-22 20:04:49 -0400

    Add unit test stubs

--- test/DocumentHelperSpec.js
+++ test/DocumentHelperSpec.js
@@ -12,7 +12,7 @@ describe('DocumentHelper', function () {
 
     describe('.extractAbstract', function () {
 
-        it('return text for simple text abstract', function () {
+        it('returns text for simple text abstract', function () {
             const xml = fs.readFileSync('./test/data/detail/efetch.xml', fsOptions);
             const abstract = DocHelper.extractAbstract(xml);
             return abstract.then(abs => {
@@ -22,6 +22,35 @@ describe('DocumentHelper', function () {
         });
 
         // TODO: more testing of `extractAbstract` with different document formats
+
+    });
+
+
+    describe('.getLinkedIdsByType', function () {
+
+        it('returns a map of uids to id-type', function () {
+
+        });
+
+        it('removes IDs without the linked ID type from the result', function () {
+
+        });
+
+    });
+
+    describe('.mergeDemographicAndSummaryResults', function () {
+
+        it('returns all items from the summary in the same order', function () {
+
+        });
+
+        it('includes demographic details where available', function () {
+
+        });
+
+        it('returns the expected attributes for each item', function () {
+
+        });
 
     });
 
